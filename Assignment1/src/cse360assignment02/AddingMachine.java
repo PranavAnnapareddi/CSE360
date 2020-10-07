d52bfe13193408e9bbd598ad90900101692bf507
PranavAnnapareddi <pranavanna@gmail.com>
2020-10-06 21:05:15 -0700

    Pushing Project into Github (Version1)

---
+++ Assignment1/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,61 @@
+package cse360assignment02;
+
+/**
+*
+* @author Pranav Annapareddi 
+* @version 1 
+* 
+* Empty Methods with Javadoc format
+*/
+public class AddingMachine {
+	
+	/**
+	 * Private integer value that holds number as program runs 
+	 * Holds the total value
+	 */
+	private int total;
+	  
+	/**
+	 * Constructor, not used
+	 */
+	  public AddingMachine () {
+	    total = 0;  // not needed - included for clarity
+	  }
+	  
+	  /**
+	   * getter method
+	   * @return the variable total
+	   */
+	  public int getTotal () {
+	    return 0;
+	  }
+	  
+	  /**
+	   * @param integer value for addition
+	   * @return unknown
+	   */
+	  public void add (int value) {
+	  }
+
+	  /**
+	   * @param integer value for subtraction
+	   * @return unknown
+	   */
+	  public void subtract (int value) {
+	  }
+
+	  /**
+	   * String format
+	   * @return Desired output for program
+	   */
+	  public String toString () {
+	    return "";
+	  }
+
+	  /**
+	   * Clears the machine
+	   */
+	  public void clear() {
+	  }
+
+}
